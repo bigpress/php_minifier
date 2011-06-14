afa1ab683404967fdf04582d927b6bcb9eafeedb
Ramon Antonio Parada <ramon@bigpress.net>
2011-06-14 14:34:08 +0200

    first commit

---
+++ cssminifier.class.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+class cssminifier {
+
+
+
+
+
+ function compress($buffer) {
+    /* remove comments */
+    $buffer = preg_replace('!/\*[^*]*\*+([^/][^*]*\*+)*/!', '', $buffer);
+    /* remove tabs, spaces, newlines, etc. */
+    $buffer = str_replace(array("\r\n", "\r", "\n", "\t", '  ', '    ', '    '), '', $buffer);
+    return $buffer;
+  }
+}
